b34f8d7ff6635e3dc91786ebcd656e002294557e
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2017-02-08 16:24:37 +0100

    create project cards

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class ProjectCard extends React.Component {
+
+  render() {
+
+    return (
+
+      <Link to={'/project/${this.props.id}'}>
+        <div className='project-card'>
+          <img src={'img/${this.props.thumbnail}'}/>
+          <h4 className='name'>{this.props.name}</h4>
+        </div>
+      </Link>
+    );
+  }
+}
