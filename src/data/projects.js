567cba2b6b3511017ab998705853b7a3a0a94a55
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2017-02-08 16:17:11 +0100

    Add image resources and update projects json

---
+++ src/data/projects.js
@@ -0,0 +1,38 @@
+const projects = [
+  {
+    'id':           'auxiliaire',
+    'name':         'Auxiliaire',
+    'description':  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ligula nisi, pharetra vitae est non, porttitor eleifend nulla. Cras condimentum malesuada euismod. Donec tincidunt tincidunt nisl eget semper. Praesent ac molestie felis. Proin sed congue ex. Nunc ut est gravida, vulputate tortor vel, hendrerit elit. Quisque id ante non dolor dictum laoreet. Vivamus mi sapien, pellentesque at elit non, pellentesque scelerisque sem. Aenean blandit quis diam vel laoreet.',
+    'mypart':       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ligula nisi, pharetra vitae est non, porttitor eleifend nulla. Cras condimentum malesuada euismod. Donec tincidunt tincidunt nisl eget semper. Praesent ac molestie felis. Proin sed congue ex. Nunc ut est gravida, vulputate tortor vel, hendrerit elit. Quisque id ante non dolor dictum laoreet. Vivamus mi sapien, pellentesque at elit non, pellentesque scelerisque sem. Aenean blandit quis diam vel laoreet.',
+    'thumbnail':    'thumbnails/auxiliaire',
+    'top':          'tops/auxiliarieTop.png',
+    'bottom':       'bottoms/auxiliarieBottom.png',
+    'link': {
+      'type': 'github',
+      'link': 'https://github.com/AuxiliaireSH/android-app'
+    },
+    'screens': [
+      'screens/auxiliaire/auxiliaire1.png',
+      'screens/auxiliaire/auxiliaire2.png',
+      'screens/auxiliaire/auxiliaire3.png'
+    ]
+  },
+  {
+    'id':           'capmap',
+    'name':         'Capmap',
+    'description':  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ligula nisi, pharetra vitae est non, porttitor eleifend nulla. Cras condimentum malesuada euismod. Donec tincidunt tincidunt nisl eget semper. Praesent ac molestie felis. Proin sed congue ex. Nunc ut est gravida, vulputate tortor vel, hendrerit elit. Quisque id ante non dolor dictum laoreet. Vivamus mi sapien, pellentesque at elit non, pellentesque scelerisque sem. Aenean blandit quis diam vel laoreet.',
+    'mypart':       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ligula nisi, pharetra vitae est non, porttitor eleifend nulla. Cras condimentum malesuada euismod. Donec tincidunt tincidunt nisl eget semper. Praesent ac molestie felis. Proin sed congue ex. Nunc ut est gravida, vulputate tortor vel, hendrerit elit. Quisque id ante non dolor dictum laoreet. Vivamus mi sapien, pellentesque at elit non, pellentesque scelerisque sem. Aenean blandit quis diam vel laoreet.',
+    'thumbnail':    'thumbnails/capmap',
+    'top':          'tops/capmapTop.png',
+    'bottom':       'bottoms/capmapBottom.png',
+    'link': {
+      'type': 'github',
+      'link': 'https://github.com/capmap/capmap'
+    },
+    'screens': [
+      'screens/capmap/map.png',
+      'screens/capmap/search.png',
+      'screens/capmap/videos.png'
+    ]
+  }
+];
