62a1d422cd6a22f6980a6493486e09fe14c3bdcd
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2017-02-10 13:24:48 +0100

    update styling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -32,28 +32,30 @@ export default class Layout extends React.Component {
 
         <div className='app-content'>{this.props.children}</div>
 
-        <div className='contact-area'>
-          <span className='contact-area-text'>
+        <div className='contact' id='contact'>
+          <div className='contact-area'>
             <p className='heading'>Contact</p>
-            <p>The easiest way to get in touch with me is via email, but you can also reach me via LinkedIn</p>
-            <div className='contact-area-text-links'>
-              <div>
-                <Link key='email' to='mailto:mikael@malmqvist.it' activeClassName="active">
-                  mikael@malmqvist.it
-                </Link>
+            <span className='contact-area-text'>
+              <p>The easiest way to get in touch with me is via email, but you can also reach me via LinkedIn</p>
+              <div className='contact-area-text-links'>
+                <div>
+                  <Link key='email' to='mailto:mikael@malmqvist.it' activeClassName="active">
+                    mikael@malmqvist.it
+                  </Link>
+                </div>
+                <div>
+                  <Link key='linkedin' to='https://www.linkedin.com/in/mikael-malmqvist-579b70a9/' activeClassName="active">
+                    LinkedIn
+                  </Link>
+                </div>
+                <div>
+                  <Link key='github' to='https://github.com/herrlax' activeClassName="active">
+                    Github
+                  </Link>
+                </div>
               </div>
-              <div>
-                <Link key='linkedin' to='https://www.linkedin.com/in/mikael-malmqvist-579b70a9/' activeClassName="active">
-                  LinkedIn
-                </Link>
-              </div>
-              <div>
-                <Link key='github' to='https://github.com/herrlax' activeClassName="active">
-                  Github
-                </Link>
-              </div>
-            </div>
-          </span>
+            </span>
+          </div>
           <img className='contact-area-img'
               src='https://d13yacurqjgara.cloudfront.net/users/42659/screenshots/3280436/cs_lewis__illustration_-01.png' />
         </div>
